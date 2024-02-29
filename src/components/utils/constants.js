977e689f8b69e8f40b129c4483f58a724e4441ee
biplab sutradhar <130878396+biplab987@users.noreply.github.com>
2024-02-29 21:53:48 +0530

    Update constants.js

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,5 +1,5 @@
 
-export const YOUR_API_KEY = "AIzaSyBEOd4Gytsc88H7mpLSaRskoeEmMXjNtLo";
+export const YOUR_API_KEY = "YOUR_API_KEY";
 
 export const youtube_api_key ="https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=48&regionCode=IN&key="+YOUR_API_KEY;
 
